5e1ee8677f3f9217bbbb923009e2aee02e059c80
Natan <xpenatan@gmail.com>
2017-05-13 20:11:48 -0300

    add getDisplayMode() #38

--- backends/gdx-backend-dragome/src/com/badlogic/gdx/backends/dragome/DragomeGraphics.java
+++ backends/gdx-backend-dragome/src/com/badlogic/gdx/backends/dragome/DragomeGraphics.java
@@ -33,72 +33,67 @@ import com.dragome.web.html.dom.w3c.HTMLCanvasElementExtension;
 import com.dragome.web.html.dom.w3c.WebGLRenderingContextExtension;
 
 /** @author xpenatan */
-public class DragomeGraphics implements Graphics
-{
+public class DragomeGraphics implements Graphics {
 	GLVersion glVersion;
 	HTMLCanvasElementExtension canvas;
 	GL20 gl;
 	String extensions;
-	float fps= 0;
-	long lastTimeStamp= System.currentTimeMillis();
-	long frameId= -1;
-	float deltaTime= 0;
-	float time= 0;
+	float fps = 0;
+	long lastTimeStamp = System.currentTimeMillis();
+	long frameId = -1;
+	float deltaTime = 0;
+	float time = 0;
 	int frames;
 	DragomeApplicationConfiguration config;
 	DragomeApplication app;
 
-	public DragomeGraphics(DragomeApplication app, DragomeApplicationConfiguration config)
-	{
-		this.app= app;
-		Element canvasElement= app.elementBySelector.getElementBySelector("canvas");
-		canvas= JsCast.castTo(canvasElement, HTMLCanvasElementExtension.class);
-		this.config= config;
+	public DragomeGraphics (DragomeApplication app, DragomeApplicationConfiguration config) {
+		this.app = app;
+		Element canvasElement = app.elementBySelector.getElementBySelector("canvas");
+		canvas = JsCast.castTo(canvasElement, HTMLCanvasElementExtension.class);
+		this.config = config;
 	}
 
-	public boolean init()
-	{
-		WebGLContextAttributes attributes= ScriptHelper.evalCasting("{premultipliedAlpha:false}", WebGLContextAttributes.class, null);
+	public boolean init () {
+		WebGLContextAttributes attributes = ScriptHelper.evalCasting("{premultipliedAlpha:false}", WebGLContextAttributes.class,
+			null);
 		attributes.setAntialias(config.antialiasing);
 		attributes.setStencil(config.stencil);
 		attributes.setAlpha(config.alpha);
 		attributes.setPremultipliedAlpha(config.premultipliedAlpha);
 		attributes.setPreserveDrawingBuffer(config.preserveDrawingBuffer);
 
-		WebGLRenderingContextExtension context= findWebGLContext(attributes);
-		if (context == null)
-			return false;
+		WebGLRenderingContextExtension context = findWebGLContext(attributes);
+		if (context == null) return false;
 
 		if (config.useDebugGL)
-			gl= new DragomeGL20Debug(context);
+			gl = new DragomeGL20Debug(context);
 		else
-			gl= new DragomeGL20(context);
+			gl = new DragomeGL20(context);
 
-		String versionString= gl.glGetString(GL20.GL_VERSION);
-		String vendorString= gl.glGetString(GL20.GL_VENDOR);
-		String rendererString= gl.glGetString(GL20.GL_RENDERER);
-//		glVersion = new GLVersion(Application.ApplicationType.WebGL, versionString, vendorString, rendererString); //FIXME needs fix
+		String versionString = gl.glGetString(GL20.GL_VERSION);
+		String vendorString = gl.glGetString(GL20.GL_VENDOR);
+		String rendererString = gl.glGetString(GL20.GL_RENDERER);
+// glVersion = new GLVersion(Application.ApplicationType.WebGL, versionString, vendorString, rendererString); //FIXME needs fix
 		return true;
 	}
 
-	private WebGLRenderingContextExtension findWebGLContext(WebGLContextAttributes attributes)
-	{
-//		String[] contextNames= new String[] { "experimental-webgl", "webgl", "moz-webgl", "webkit-webgl", "webkit-3d" };
-//		for (String contextName : contextNames)
-//		{
-//			Object context= canvas.getContext(contextName, attributes);
-//			if (context != null)
-//				return (WebGLRenderingContextExtension) context;
-//		}
+	private WebGLRenderingContextExtension findWebGLContext (WebGLContextAttributes attributes) {
+// String[] contextNames= new String[] { "experimental-webgl", "webgl", "moz-webgl", "webkit-webgl", "webkit-3d" };
+// for (String contextName : contextNames)
+// {
+// Object context= canvas.getContext(contextName, attributes);
+// if (context != null)
+// return (WebGLRenderingContextExtension) context;
+// }
 
-		String[] contextNames= new String[] { "experimental-webgl", "webgl", "moz-webgl", "webkit-webgl", "webkit-3d" };
+		String[] contextNames = new String[] {"experimental-webgl", "webgl", "moz-webgl", "webkit-webgl", "webkit-3d"};
 		ScriptHelper.put("canvas", canvas, this);
 		ScriptHelper.put("attr", attributes, this);
-		for (String contextName : contextNames)
-		{
+		for (String contextName : contextNames) {
 			ScriptHelper.put("contextName", contextName, this);
 			Object con = ScriptHelper.eval("canvas.node.getContext(contextName, attr.node)", this);
-			if(con != null) {
+			if (con != null) {
 				ScriptHelper.put("con", con, null);
 				return ScriptHelper.evalCasting("con", WebGLRenderingContextExtension.class, null);
 			}
@@ -106,253 +101,219 @@ public class DragomeGraphics implements Graphics
 		return null;
 	}
 
-	public void update()
-	{
-		long currTimeStamp= System.currentTimeMillis();
-		deltaTime= (currTimeStamp - lastTimeStamp) / 1000.0f;
-		lastTimeStamp= currTimeStamp;
-		time+= deltaTime;
+	public void update () {
+		long currTimeStamp = System.currentTimeMillis();
+		deltaTime = (currTimeStamp - lastTimeStamp) / 1000.0f;
+		lastTimeStamp = currTimeStamp;
+		time += deltaTime;
 		frames++;
-		if (time > 1)
-		{
-			this.fps= frames;
-			time= 0;
-			frames= 0;
+		if (time > 1) {
+			this.fps = frames;
+			time = 0;
+			frames = 0;
 		}
 	}
 
 	@Override
-	public boolean isGL30Available()
-	{
+	public boolean isGL30Available () {
 		return false;
 	}
 
 	@Override
-	public GL20 getGL20()
-	{
+	public GL20 getGL20 () {
 		return gl;
 	}
 
 	@Override
-	public GL30 getGL30()
-	{
+	public GL30 getGL30 () {
 		return null;
 	}
 
 	@Override
-	public int getWidth()
-	{
+	public int getWidth () {
 		return canvas.getWidth();
 	}
 
 	@Override
-	public int getHeight()
-	{
+	public int getHeight () {
 		return canvas.getHeight();
 	}
 
 	@Override
-	public int getBackBufferWidth()
-	{
+	public int getBackBufferWidth () {
 		return canvas.getWidth();
 	}
 
 	@Override
-	public int getBackBufferHeight()
-	{
+	public int getBackBufferHeight () {
 		return canvas.getHeight();
 	}
 
 	@Override
-	public long getFrameId()
-	{
+	public long getFrameId () {
 		return frameId;
 	}
 
 	@Override
-	public float getDeltaTime()
-	{
+	public float getDeltaTime () {
 		return deltaTime;
 	}
 
 	@Override
-	public float getRawDeltaTime()
-	{
+	public float getRawDeltaTime () {
 		return deltaTime;
 	}
 
 	@Override
-	public int getFramesPerSecond()
-	{
-		return (int) fps;
+	public int getFramesPerSecond () {
+		return (int)fps;
 	}
 
 	@Override
-	public GraphicsType getType()
-	{
+	public GraphicsType getType () {
 		return GraphicsType.WebGL;
 	}
 
 	@Override
-	public float getPpiX()
-	{
+	public float getPpiX () {
 		return 96;
 	}
 
 	@Override
-	public float getPpiY()
-	{
+	public float getPpiY () {
 		return 96;
 	}
 
 	@Override
-	public float getPpcX()
-	{
+	public float getPpcX () {
 		return 96 / 2.54f;
 	}
 
 	@Override
-	public float getPpcY()
-	{
+	public float getPpcY () {
 		return 96 / 2.54f;
 	}
 
 	@Override
-	public float getDensity()
-	{
+	public float getDensity () {
 		return 96.0f / 160;
 	}
 
 	@Override
-	public boolean supportsDisplayModeChange()
-	{
+	public boolean supportsDisplayModeChange () {
 		return false;
 	}
 
 	@Override
-	public Monitor getPrimaryMonitor()
-	{
+	public Monitor getPrimaryMonitor () {
 		return null;
 	}
 
 	@Override
-	public Monitor getMonitor()
-	{
+	public Monitor getMonitor () {
 		return null;
 	}
 
 	@Override
-	public Monitor[] getMonitors()
-	{
+	public Monitor[] getMonitors () {
 		return null;
 	}
 
 	@Override
-	public DisplayMode[] getDisplayModes()
-	{
-		return null;
+	public DisplayMode[] getDisplayModes () {
+		return new DisplayMode[] {new DisplayMode(getScreenWidth(), getScreenHeight(), 60, 8) {}};
+	}
+
+	private int getScreenWidth () {
+		return ScriptHelper.evalInt("window.screen.width", this);
+	}
+
+	private int getScreenHeight () {
+		return ScriptHelper.evalInt("window.screen.height", this);
 	}
 
 	@Override
-	public DisplayMode[] getDisplayModes(Monitor monitor)
-	{
-		return null;
+	public DisplayMode[] getDisplayModes (Monitor monitor) {
+		return getDisplayModes();
 	}
 
 	@Override
-	public DisplayMode getDisplayMode()
-	{
-		return null;
+	public DisplayMode getDisplayMode () {
+		return new DisplayMode(getScreenWidth(), getScreenHeight(), 60, 8) {};
 	}
 
 	@Override
-	public DisplayMode getDisplayMode(Monitor monitor)
-	{
-		return null;
+	public DisplayMode getDisplayMode (Monitor monitor) {
+		return getDisplayMode();
 	}
 
 	@Override
-	public boolean setFullscreenMode(DisplayMode displayMode)
-	{
+	public boolean setFullscreenMode (DisplayMode displayMode) {
 		return false;
 	}
 
 	@Override
-	public boolean setWindowedMode(int width, int height)
-	{
+	public boolean setWindowedMode (int width, int height) {
 		return false;
 	}
 
 	@Override
-	public void setTitle(String title)
-	{
+	public void setTitle (String title) {
 	}
 
 	@Override
-	public void setVSync(boolean vsync)
-	{
+	public void setVSync (boolean vsync) {
 	}
 
 	@Override
-	public BufferFormat getBufferFormat()
-	{
+	public BufferFormat getBufferFormat () {
 		return new BufferFormat(8, 8, 8, 0, 16, config.stencil ? 8 : 0, 0, false);
 	}
 
 	@Override
-	public boolean supportsExtension(String extension)
-	{
-		if (extensions == null)
-			extensions= Gdx.gl.glGetString(GL20.GL_EXTENSIONS);
+	public boolean supportsExtension (String extension) {
+		if (extensions == null) extensions = Gdx.gl.glGetString(GL20.GL_EXTENSIONS);
 		return extensions.contains(extension);
 	}
 
 	@Override
-	public void setContinuousRendering(boolean isContinuous)
-	{
+	public void setContinuousRendering (boolean isContinuous) {
 	}
 
 	@Override
-	public boolean isContinuousRendering()
-	{
+	public boolean isContinuousRendering () {
 		return true;
 	}
 
 	@Override
-	public void requestRendering()
-	{
+	public void requestRendering () {
 	}
 
 	@Override
-	public boolean isFullscreen()
-	{
+	public boolean isFullscreen () {
 		return false;
 	}
 
 	@Override
-	public Cursor newCursor(Pixmap pixmap, int xHotspot, int yHotspot)
-	{
+	public Cursor newCursor (Pixmap pixmap, int xHotspot, int yHotspot) {
 		return null;
 	}
 
 	@Override
-	public void setCursor(Cursor cursor)
-	{
+	public void setCursor (Cursor cursor) {
 	}
 
 	@Override
-	public void setSystemCursor(SystemCursor systemCursor)
-	{
+	public void setSystemCursor (SystemCursor systemCursor) {
 	}
 
-	public GLVersion getGLVersion()
-	{
+	public GLVersion getGLVersion () {
 		return glVersion;
 	}
 
-	public void setUndecorated(boolean undecorated) {
+	public void setUndecorated (boolean undecorated) {
 	}
 
-	public void setResizable(boolean resizable) {
+	public void setResizable (boolean resizable) {
 	}
 }
