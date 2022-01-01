a7278313c3a547add0aa694304f35a2df0fb4cd1
Natan <xpenatan@gmail.com>
2022-01-01 20:08:12 -0300

    Add filter to exclude class

--- backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/plugins/TeaClassFilter.java
+++ backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/plugins/TeaClassFilter.java
@@ -4,10 +4,35 @@ import org.teavm.model.FieldReference;
 import org.teavm.model.MethodReference;
 import org.teavm.vm.spi.ElementFilter;
 
+import java.util.ArrayList;
+
 public class TeaClassFilter implements ElementFilter {
 
+	private static ArrayList<String> clazzList = new ArrayList();
+
+	/**
+	 * my.package.ClassName
+	 * or
+	 * my.package
+	 */
+	public static void addClassToExclude(String className) {
+		clazzList.add(className);
+	}
+
+	private static boolean containsClass(String className) {
+		for(int i = 0; i < clazzList.size(); i++) {
+			String excludedClass = clazzList.get(i);
+			if(className.contains(excludedClass))
+				return true;
+		}
+		return false;
+	}
+
 	@Override
-	public boolean acceptClass (String className) {
+	public boolean acceptClass (String fullClassName) {
+		if(containsClass(fullClassName)) {
+			return false;
+		}
 		return true;
 	}
 
