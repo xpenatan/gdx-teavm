9d497b49672c0a5f44a27093ce86109c01233a2c
Natan <xpenatan@gmail.com>
2022-01-03 13:33:08 -0300

    Parser is not static

--- extensions/gdx-bullet/gdx-bullet-build/src/main/java/com/github/xpenatan/gdx/html5/bullet/Main.java
+++ extensions/gdx-bullet/gdx-bullet-build/src/main/java/com/github/xpenatan/gdx/html5/bullet/Main.java
@@ -9,10 +9,6 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) throws Exception {
         String basePath = new File(".").getAbsolutePath();
-        generate(new TeaVMCodeParser(), basePath + "./gdx-bullet-base/src", "../gdx-bullet-teavm/src");
-    }
-
-    public static void generate(TeaVMCodeParser wrapper, String sourceDir, String genDir)  throws Exception {
-        new JParser().generate(wrapper, sourceDir, genDir, null);
+        JParser.generate(new TeaVMCodeParser(), basePath + "./gdx-bullet-base/src", "../gdx-bullet-teavm/src", null);
     }
 }
