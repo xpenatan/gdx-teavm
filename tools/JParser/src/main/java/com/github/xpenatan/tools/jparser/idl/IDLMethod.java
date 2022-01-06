9e721a3931688368d69de89b21b87d2f22004bc6
Natan <xpenatan@gmail.com>
2022-01-06 20:02:50 -0300

    Fix parser

--- tools/JParser/src/main/java/com/github/xpenatan/tools/jparser/idl/IDLMethod.java
+++ tools/JParser/src/main/java/com/github/xpenatan/tools/jparser/idl/IDLMethod.java
@@ -16,16 +16,34 @@ public class IDLMethod {
         paramsLine = IDLMethod.setParameters(line, parameters);
         int index = line.indexOf("(");
         String leftSide = line.substring(0, index).trim();
-        int startIndex = leftSide.indexOf("[");
-        int endIndex = leftSide.indexOf("]");
-        if(startIndex != -1 && endIndex != -1) {
-            String tags = line.substring(startIndex, endIndex + 1);
-            leftSide = leftSide.replace(tags, "").trim();
+        int endIndex = getLastIndex(leftSide);
+        if(endIndex != -1) {
+            leftSide = line.substring(endIndex + 1, index).trim();
         }
 
         String[] s = leftSide.split(" ");
         returnType = s[0];
         name = s[1];
+    }
+
+    private int getLastIndex(String leftSide) {
+        int startIndex = leftSide.indexOf("[");
+        if(startIndex != -1) {
+            int count = 0;
+            for(int i = startIndex; i < leftSide.length(); i++) {
+                char c = leftSide.charAt(i);
+                if(c == '[') {
+                    count++;
+                }
+                else if(c == ']') {
+                    count--;
+                }
+                if(count == 0) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
     static String setParameters(String line, ArrayList<IDLParameter> out) {
