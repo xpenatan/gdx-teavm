276445c76bbbc2ca60d16c91771063a04af25459
Natan <xpenatan@gmail.com>
2022-01-06 00:41:15 -0300

    add helper method

--- tools/JParser/src/main/java/com/github/xpenatan/tools/jparser/idl/IDLFile.java
+++ tools/JParser/src/main/java/com/github/xpenatan/tools/jparser/idl/IDLFile.java
@@ -5,4 +5,15 @@ import java.util.ArrayList;
 /** @author xpenatan */
 public class IDLFile {
     public final ArrayList<IDLClass> classArray = new ArrayList<>();
+
+    public IDLClass getClass(String name) {
+        int size = classArray.size();
+        for(int i = 0; i < size; i++) {
+            IDLClass idlClass = classArray.get(i);
+            if(idlClass.interfaceName.equals(name)) {
+                return idlClass;
+            }
+        }
+        return null;
+    }
 }
