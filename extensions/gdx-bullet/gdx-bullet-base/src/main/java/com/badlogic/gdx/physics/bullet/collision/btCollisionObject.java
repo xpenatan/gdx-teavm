cc77539ba3e9b4904d923db967b5a4d81a9c6953
Natan <xpenatan@gmail.com>
2022-01-16 15:47:16 -0300

    Keep shape ref

--- extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btCollisionObject.java
+++ extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btCollisionObject.java
@@ -6,4 +6,20 @@ import com.badlogic.gdx.physics.bullet.BulletBase;
  * @author xpenatan
  */
 public class btCollisionObject extends BulletBase {
+    btCollisionShape collisionShape;
+
+    public void setCollisionShape (btCollisionShape collisionShape) {
+        this.collisionShape = collisionShape;
+        setCollisionShape(cPointer, collisionShape != null ? collisionShape.getCPointer() : 0);
+    }
+
+    /*[-teaVM;-NATIVE]
+        var jsObj = Bullet.wrapPointer(addr, Bullet.btCollisionObject);
+        jsObj.setCollisionShape(collisionShapeAddr);
+     */
+    private static native void setCollisionShape(long addr, long collisionShapeAddr);
+
+    public btCollisionShape getCollisionShape () {
+        return collisionShape;
+    }
 }
