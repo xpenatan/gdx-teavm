0e2b8dfef69c3bf54f7d493f1c0dd7309b364da2
Natan <xpenatan@gmail.com>
2022-01-20 00:31:26 -0300

    init compound shape

--- extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btCompoundShape.java
+++ extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btCompoundShape.java
@@ -4,4 +4,25 @@ package com.badlogic.gdx.physics.bullet.collision;
  * @author xpenatan
  */
 public class btCompoundShape extends btCollisionShape {
+
+    public btCompoundShape() {
+        initObject(createNative(), true);
+    }
+
+    /*[-teaVM;-NATIVE]
+        var jsObj = new Bullet.btCompoundShape(radius);
+        return Bullet.getPointer(jsObj);
+     */
+    private static native long createNative();
+
+    @Override
+    protected void deleteNative() {
+        deleteNative(cPointer);
+    }
+
+    /*[-teaVM;-NATIVE]
+        var jsObj = Bullet.wrapPointer(addr, Bullet.btCompoundShape);
+        Bullet.destroy(jsObj);
+     */
+    private static native void deleteNative(long addr);
 }
