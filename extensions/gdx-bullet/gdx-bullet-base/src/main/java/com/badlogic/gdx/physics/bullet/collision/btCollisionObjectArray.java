0a5f96d5c9c708cc897b0f1b12a916f4263e5c53
Natan <xpenatan@gmail.com>
2022-01-19 22:20:55 -0300

    Add custom method TODO

--- extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btCollisionObjectArray.java
+++ extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btCollisionObjectArray.java
@@ -3,4 +3,9 @@ package com.badlogic.gdx.physics.bullet.collision;
 import com.badlogic.gdx.physics.bullet.BulletBase;
 
 public class btCollisionObjectArray extends BulletBase {
+
+    public btCollisionObject atConst (int n) {
+        //TODO impl
+        return null;
+    }
 }
