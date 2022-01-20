1a9c5a04cbf5f083e2829c81a2362a4e989c6c03
Natan <xpenatan@gmail.com>
2022-01-20 09:12:35 -0300

    remove import

--- backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/TeaBuilder.java
+++ backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/TeaBuilder.java
@@ -18,7 +18,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.*;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
