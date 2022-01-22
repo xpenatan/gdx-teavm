193c0edae466180a56520f65f9b47ec2ba6fb18d
Natan <xpenatan@gmail.com>
2022-01-22 18:21:59 -0300

    Add fixed constraint

--- extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/dynamics/btFixedConstraint.java
+++ extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/dynamics/btFixedConstraint.java
@@ -6,12 +6,9 @@ import com.badlogic.gdx.physics.bullet.linearmath.btTransform;
 /**
  * @author xpenatan
  */
-public class btGeneric6DofSpring2Constraint extends btTypedConstraint {
+public class btFixedConstraint extends btGeneric6DofSpring2Constraint {
 
-    protected btGeneric6DofSpring2Constraint() {
-    }
-
-    public btGeneric6DofSpring2Constraint (btRigidBody rbA, btRigidBody rbB, Matrix4 frameInA, Matrix4 frameInB) {
+    public btFixedConstraint (btRigidBody rbA, btRigidBody rbB, Matrix4 frameInA, Matrix4 frameInB) {
         btTransform btframeInA = btTransform.TEMP_0;
         btTransform btframeInB = btTransform.TEMP_1;
         btTransform.convert(frameInA, btframeInA);
@@ -20,7 +17,7 @@ public class btGeneric6DofSpring2Constraint extends btTypedConstraint {
     }
 
     /*[-teaVM;-NATIVE]
-        var jsObj = new Bullet.btGeneric6DofSpring2Constraint(rbAAddr, rbBAddr, frameInAAddr, frameInBAddr);
+        var jsObj = new Bullet.btFixedConstraint(rbAAddr, rbBAddr, frameInAAddr, frameInBAddr);
         return Bullet.getPointer(jsObj);
      */
     private static native long createNative(long rbAAddr, long rbBAddr, long frameInAAddr, long frameInBAddr);
@@ -31,7 +28,7 @@ public class btGeneric6DofSpring2Constraint extends btTypedConstraint {
     }
 
     /*[-teaVM;-NATIVE]
-        var jsObj = Bullet.wrapPointer(addr, Bullet.btGeneric6DofSpring2Constraint);
+        var jsObj = Bullet.wrapPointer(addr, Bullet.btFixedConstraint);
         Bullet.destroy(jsObj);
      */
     private static native void deleteNative(long addr);
