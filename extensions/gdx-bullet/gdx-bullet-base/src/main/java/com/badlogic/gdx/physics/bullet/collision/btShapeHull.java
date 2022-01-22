5c9bcb7cae91653e94e2c46220b453d68f73f18c
Natan <xpenatan@gmail.com>
2022-01-22 18:23:35 -0300

    Add shape hull

---
+++ extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btShapeHull.java
@@ -0,0 +1,30 @@
+package com.badlogic.gdx.physics.bullet.collision;
+
+import com.badlogic.gdx.physics.bullet.BulletBase;
+
+/**
+ * @author xpenatan
+ */
+public class btShapeHull extends BulletBase {
+
+    public btShapeHull (btConvexShape shape) {
+        initObject(createNative(shape.getCPointer()), true);
+    }
+
+    /*[-teaVM;-NATIVE]
+        var jsObj = new Bullet.btShapeHull(shapeAddr);
+        return Bullet.getPointer(jsObj);
+     */
+    private static native long createNative(long shapeAddr);
+
+    @Override
+    protected void deleteNative() {
+        deleteNative(cPointer);
+    }
+
+    /*[-teaVM;-NATIVE]
+        var jsObj = Bullet.wrapPointer(addr, Bullet.btShapeHull);
+        Bullet.destroy(jsObj);
+     */
+    private static native void deleteNative(long addr);
+}
