8a5e3e57b75afb7051a9c31899a0ff4cdd3dd818
Natan <xpenatan@gmail.com>
2022-01-22 18:16:39 -0300

    Add Scalar type

---
+++ extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/PHY_ScalarType.java
@@ -0,0 +1,16 @@
+package com.badlogic.gdx.physics.bullet.collision;
+
+/**
+ * @author xpenatan
+ */
+public class PHY_ScalarType {
+    public static final int PHY_FLOAT = 0;
+    public static final int PHY_DOUBLE = 1;
+    public static final int PHY_INTEGER = 2;
+    public static final int PHY_SHORT = 3;
+    public static final int PHY_FIXEDPOINT88 = 4;
+    public static final int PHY_UCHAR = 5;
+
+    public PHY_ScalarType() {
+    }
+}
