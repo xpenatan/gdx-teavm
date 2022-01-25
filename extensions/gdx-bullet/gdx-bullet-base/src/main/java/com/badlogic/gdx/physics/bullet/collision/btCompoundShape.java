374e33551fe714b5642d4bdf8c149fadbdc74282
Natan <xpenatan@gmail.com>
2022-01-25 15:11:53 -0300

    Add method

--- extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btCompoundShape.java
+++ extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btCompoundShape.java
@@ -1,5 +1,8 @@
 package com.badlogic.gdx.physics.bullet.collision;
 
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.physics.bullet.linearmath.btTransform;
+
 /**
  * @author xpenatan
  */
@@ -25,4 +28,17 @@ public class btCompoundShape extends btCollisionShape {
         Bullet.destroy(jsObj);
      */
     private static native void deleteNative(long addr);
+
+    public void addChildShape (Matrix4 localTransform, btCollisionShape shape) {
+        // Required. it's called from Bullet class
+        btTransform.convert(localTransform, btTransform.TEMP_0);
+        btTransform btLocalTransform = btTransform.TEMP_0;
+        addChildShapeNATIVE((int) cPointer, (int) btLocalTransform.getCPointer(), (int) shape.getCPointer());
+    }
+
+    /*[-teaVM;-NATIVE]
+        var jsObj = Bullet.wrapPointer(addr, Bullet.btCompoundShape);
+        jsObj.addChildShape(localTransformAddr, shapeAddr);
+     */
+    private static native void addChildShapeNATIVE(long addr, long localTransformAddr, long shapeAddr);
 }
