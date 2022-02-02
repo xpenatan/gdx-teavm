a53d2b1fa113f34d269e5079e6e4a15903afc4aa
Natan <xpenatan@gmail.com>
2022-02-01 21:08:55 -0300

    Some changes

--- extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/LocalRayResult.java
+++ extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/LocalRayResult.java
@@ -1,9 +1,60 @@
 package com.badlogic.gdx.physics.bullet.collision;
 
 import com.badlogic.gdx.physics.bullet.BulletBase;
+import com.badlogic.gdx.physics.bullet.linearmath.btVector3;
 
 /**
  * @author xpenatan
  */
 public class LocalRayResult extends BulletBase {
+
+    public static LocalRayResult temp01 = new LocalRayResult();
+
+    public LocalRayResult() {
+        initObject(0, false);
+    }
+
+    public btCollisionObject getCollisionObject () {
+        btCollisionObject.temp01.setPointer(getCollisionObject(cPointer));
+        return btCollisionObject.temp01;
+    }
+
+    /*[-teaVM;-NATIVE]
+        var jsObj = Bullet.wrapPointer(addr, Bullet.LocalRayResult);
+        var colJSObj = jsObj.get_m_collisionObject();
+        return Bullet.getPointer(colJSObj);
+     */
+    private static native long getCollisionObject(long addr);
+
+    public btVector3 getHitNormalLocal () {
+        btVector3.emptyTransform.setPointer(getHitNormalLocal(cPointer));
+        return btVector3.emptyTransform;
+    }
+
+    /*[-teaVM;-NATIVE]
+        var jsObj = Bullet.wrapPointer(addr, Bullet.LocalRayResult);
+        var vec3JSObj = jsObj.get_m_hitNormalLocal();
+        return Bullet.getPointer(vec3JSObj);
+     */
+    private static native long getHitNormalLocal(long addr);
+
+    public void setHitFraction (float value) {
+        setHitFraction(cPointer, value);
+    }
+
+    /*[-teaVM;-NATIVE]
+        var jsObj = Bullet.wrapPointer(addr, Bullet.LocalRayResult);
+        jsObj.set_m_hitFraction(value);
+     */
+    private static native void setHitFraction(long addr, float value);
+
+    public float getHitFraction () {
+        return getHitFraction(cPointer);
+    }
+
+    /*[-teaVM;-NATIVE]
+        var jsObj = Bullet.wrapPointer(addr, Bullet.LocalRayResult);
+        return jsObj.get_m_hitFraction();
+     */
+    private static native float getHitFraction(long addr);
 }
--- extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/dynamics/btRigidBody.java
+++ extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/dynamics/btRigidBody.java
@@ -1,6 +1,7 @@
 package com.badlogic.gdx.physics.bullet.dynamics;
 
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.bullet.BulletBase;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
 import com.badlogic.gdx.physics.bullet.linearmath.btMotionState;
@@ -11,11 +12,20 @@ import com.badlogic.gdx.physics.bullet.linearmath.btVector3;
  */
 public class btRigidBody extends btCollisionObject {
 
+
+    public btRigidBody (btRigidBodyConstructionInfo constructionInfo) {
+        super(0);
+        btVector3 out = new btVector3();
+        btVector3.convert(constructionInfo.localInertia, out);
+        initObject(createNative(constructionInfo.mass, (int)constructionInfo.motionStateAddr, (int)constructionInfo.collisionShapeAddr, out.getCPointer()), true);
+    }
+
     public btRigidBody(float mass, btMotionState motionState, btCollisionShape collisionShape, Vector3 localInertia) {
-        btVector3 out = btVector3.TEMP_0;
-        btVector3.convert(localInertia, out);
+        super(0);
+        btVector3 out = new btVector3();
+        btVector3.convert(localInertia != null ? localInertia : btVector3.TEMP_GDX_01.setZero(), out);
         long motionStatePointer = motionState != null ? motionState.getCPointer() : 0;
-        long shapePointer = motionState != null ? collisionShape.getCPointer() : 0;
+        long shapePointer = collisionShape != null ? collisionShape.getCPointer() : 0;
         initObject(createNative(mass, motionStatePointer,shapePointer, out.getCPointer()), true);
     }
 
@@ -39,4 +49,24 @@ public class btRigidBody extends btCollisionObject {
      */
     private static native void deleteNative(long addr);
 
+    static public class btRigidBodyConstructionInfo extends BulletBase {
+
+        public float mass;
+        public long motionStateAddr;
+        public long collisionShapeAddr;
+        public Vector3 localInertia = new Vector3();
+
+        public btRigidBodyConstructionInfo (float mass, btMotionState motionState, btCollisionShape collisionShape, Vector3 localInertia) {
+            this.mass = mass;
+            this.motionStateAddr = motionState != null ? motionState.getCPointer() : 0;
+            this.collisionShapeAddr = collisionShape != null ? collisionShape.getCPointer() : 0;
+            this.localInertia.set(localInertia);
+        }
+
+        public btRigidBodyConstructionInfo (float mass, btMotionState motionState, btCollisionShape collisionShape) {
+            this.mass = mass;
+            this.motionStateAddr = motionState != null ? motionState.getCPointer() : 0;
+            this.collisionShapeAddr = collisionShape != null ? collisionShape.getCPointer() : 0;
+        }
+    }
 }
