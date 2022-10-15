5402663774e4ef6cf9183dde66960d560fb85550
Natan <xpenatan@gmail.com>
2022-10-15 17:20:59 -0300

    small idl attribute impl

--- tools/JParser/src/main/java/com/github/xpenatan/tools/jparser/idl/IDLAttribute.java
+++ tools/JParser/src/main/java/com/github/xpenatan/tools/jparser/idl/IDLAttribute.java
@@ -3,9 +3,13 @@ package com.github.xpenatan.tools.jparser.idl;
 /** @author xpenatan */
 public class IDLAttribute {
     public String line;
+    public String type;
+    public String name;
 
     public void initAttribute(String line) {
         this.line = line;
-        // TODO impl
+        String[] s = line.replace("attribute", "").replace(";", "").trim().split(" ");
+        type = s[0];
+        name = s[1];
     }
 }
