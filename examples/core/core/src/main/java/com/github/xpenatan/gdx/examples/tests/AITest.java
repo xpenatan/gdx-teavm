af52accd2a94a5fe7f2fa914f2965abc43c1fb9e
Natan <xpenatan@gmail.com>
2022-12-02 08:33:58 -0300

    Add empty ai test

---
+++ examples/core/core/src/main/java/com/github/xpenatan/gdx/examples/tests/AITest.java
@@ -0,0 +1,12 @@
+package com.github.xpenatan.gdx.examples.tests;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class AITest extends ApplicationAdapter {
+
+    @Override
+    public void create() {
+
+
+    }
+}
