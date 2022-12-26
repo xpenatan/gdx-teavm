5c488ef47458f4f08c901496c5edc3abe411cec4
Natan <xpenatan@gmail.com>
2022-12-26 11:07:40 -0300

    Bullet - add module

--- extensions/gdx-bullet/gdx-bullet-build/src/main/java/com/github/xpenatan/gdx/html5/bullet/Main.java
+++ extensions/gdx-bullet/gdx-bullet-build/src/main/java/com/github/xpenatan/gdx/html5/bullet/Main.java
@@ -3,6 +3,8 @@ package com.github.xpenatan.gdx.html5.bullet;
 import com.github.xpenatan.jparser.core.JParser;
 import com.github.xpenatan.jparser.core.idl.IDLFile;
 import com.github.xpenatan.jparser.core.idl.IDLParser;
+import com.github.xpenatan.jparser.cpp.CPPBuildHelper;
+import com.github.xpenatan.jparser.cpp.FileCopyHelper;
 import java.io.File;
 
 public class Main {
@@ -12,5 +14,14 @@ public class Main {
 
         String basePath = new File(".").getAbsolutePath();
         JParser.generate(new BulletCodeParser(idlFile), basePath + "./gdx-bullet-base/src", "../gdx-bullet-teavm/src", null);
+
+        String libName = "gdx-bullet";
+        String cppPath = new File("./jni/").getCanonicalPath();
+        String buildPath = cppPath + "/build/c++/";
+        FileCopyHelper.copyDir(cppPath + "/bullet/src/", buildPath + "/src");
+
+        String [] flags = new String[1];
+        flags[0] = " -DBT_USE_INVERSE_DYNAMICS_WITH_BULLET2";
+        CPPBuildHelper.build(libName, buildPath, flags);
     }
 }
