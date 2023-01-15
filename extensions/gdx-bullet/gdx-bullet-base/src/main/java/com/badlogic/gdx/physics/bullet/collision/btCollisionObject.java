8066e39a0b7445103fca6b829f7d9ed76045a756
Natan <xpenatan@gmail.com>
2023-01-15 13:57:55 -0300

    Remove some methods

--- extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btCollisionObject.java
+++ extensions/gdx-bullet/gdx-bullet-base/src/main/java/com/badlogic/gdx/physics/bullet/collision/btCollisionObject.java
@@ -1,11 +1,7 @@
 package com.badlogic.gdx.physics.bullet.collision;
 
-import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.BulletBase;
 import com.badlogic.gdx.physics.bullet.linearmath.VoidPtr;
-import com.badlogic.gdx.physics.bullet.linearmath.btTransform;
-import com.badlogic.gdx.physics.bullet.linearmath.btVector3;
 
 /**
  * @author xpenatan
@@ -91,71 +87,6 @@ public class btCollisionObject extends BulletBase {
         return intPointer;
     */
     private static native long getUserPointer(long addr);
-
-    public void getWorldTransform(Matrix4 out) {
-        // Gdx method
-        int worldTransformAddr = (int)getWorldTransformAddr(cPointer);
-        btTransform.convert(worldTransformAddr, out);
-    }
-
-    /*[-C++;-NATIVE]
-        btCollisionObject* nativeObject = (btCollisionObject*)addr;
-        return (jlong)&nativeObject->getWorldTransform();
-    */
-    /*[-teaVM;-NATIVE]
-        var jsObj = Bullet.wrapPointer(addr, Bullet.btCollisionObject);
-        var jsTransform = jsObj.getWorldTransform();
-        return Bullet.getPointer(jsTransform);
-    */
-    private static native long getWorldTransformAddr(long addr);
-
-    public void setInterpolationAngularVelocity(btVector3 angvel) {
-        setInterpolationAngularVelocityNATIVE(cPointer, (int)angvel.getCPointer());
-    }
-
-    /*[-C++;-NATIVE]
-        btCollisionObject* nativeObject = (btCollisionObject*)addr;
-        nativeObject->setInterpolationAngularVelocity(*((btVector3*)angvelAddr));
-    */
-    private static native void setInterpolationAngularVelocityNATIVE(long addr, long angvelAddr);
-
-    public void setInterpolationAngularVelocity(Vector3 angvelGDX) {
-        btVector3.convert(angvelGDX, btVector3.TEMP_0);
-        btVector3 angvel = btVector3.TEMP_0;
-        setInterpolationAngularVelocityNATIVE(cPointer, (int)angvel.getCPointer());
-    }
-
-    public void setInterpolationLinearVelocity(btVector3 linvel) {
-        setInterpolationLinearVelocityNATIVE(cPointer, (int)linvel.getCPointer());
-    }
-
-    /*[-C++;-NATIVE]
-        btCollisionObject* nativeObject = (btCollisionObject*)addr;
-        nativeObject->setInterpolationLinearVelocity(*((btVector3*)linvelAddr));
-    */
-    private static native void setInterpolationLinearVelocityNATIVE(long addr, long linvelAddr);
-
-    public void setInterpolationLinearVelocity(Vector3 linvelGDX) {
-        btVector3.convert(linvelGDX, btVector3.TEMP_0);
-        btVector3 linvel = btVector3.TEMP_0;
-        setInterpolationLinearVelocityNATIVE(cPointer, (int)linvel.getCPointer());
-    }
-
-    public void setInterpolationWorldTransform(btTransform trans) {
-        setInterpolationWorldTransformNATIVE(cPointer, (int)trans.getCPointer());
-    }
-
-    /*[-C++;-NATIVE]
-        btCollisionObject* nativeObject = (btCollisionObject*)addr;
-        nativeObject->setInterpolationWorldTransform(*((btTransform*)transAddr));
-    */
-    private static native void setInterpolationWorldTransformNATIVE(long addr, long transAddr);
-
-    public void setInterpolationWorldTransform(Matrix4 transGDX) {
-        btTransform.convert(transGDX, btTransform.TEMP_0);
-        btTransform trans = btTransform.TEMP_0;
-        setInterpolationWorldTransformNATIVE(cPointer, (int)trans.getCPointer());
-    }
 
     public final static class CollisionFlags {
         public final static int CF_STATIC_OBJECT = 1;
