ddb56fa0141a829df65f57aac1a6d7ae5f1c57f5
Natan <xpenatan@gmail.com>
2023-02-09 11:42:20 -0300

    Add temp imgui loading

--- backends/backend-teavm/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/TeaJSApplication.java
+++ backends/backend-teavm/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/TeaJSApplication.java
@@ -3,7 +3,6 @@ package com.github.xpenatan.gdx.backends.teavm;
 import com.github.xpenatan.gdx.backends.web.AssetLoaderListener;
 import com.github.xpenatan.gdx.backends.web.WebApplication;
 import com.github.xpenatan.gdx.backends.web.WebJSApplication;
-import com.github.xpenatan.gdx.backends.web.preloader.Blob;
 import com.github.xpenatan.gdx.backends.web.preloader.Preloader;
 
 public class TeaJSApplication implements WebJSApplication {
@@ -20,8 +19,28 @@ public class TeaJSApplication implements WebJSApplication {
         initBox2DPhysicsWasm(preloader);
     }
 
+    @Override
+    public void initImGui(WebApplication application) {
+        //TODO script loading should be inside lib and not application
+        Preloader preloader = application.getPreloader();
+        initImGuiWasm(preloader);
+    }
+
     private void initBulletPhysicsWasm(Preloader preloader) {
         preloader.loadScript(false, "bullet.wasm.js", new AssetLoaderListener<Object>() {
+            @Override
+            public boolean onSuccess(String url, Object result) {
+                return true;
+            }
+
+            @Override
+            public void onFailure(String url) {
+            }
+        });
+    }
+
+    private void initImGuiWasm(Preloader preloader) {
+        preloader.loadScript(false, "imgui.js", new AssetLoaderListener<Object>() {
             @Override
             public boolean onSuccess(String url, Object result) {
                 return true;
