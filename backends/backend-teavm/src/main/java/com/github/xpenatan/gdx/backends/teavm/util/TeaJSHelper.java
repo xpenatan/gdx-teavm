962bbe3f7113d8d93036131be7133d18947eb2ca
Natan <xpenatan@gmail.com>
2023-02-13 10:51:39 -0300

    Remove imports

--- backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/util/TeaJSHelper.java
+++ backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/util/TeaJSHelper.java
@@ -1,14 +1,7 @@
 package com.github.xpenatan.gdx.backends.teavm.util;
 
-import com.github.xpenatan.gdx.backends.teavm.dom.DocumentWrapper;
-import com.github.xpenatan.gdx.backends.teavm.dom.HTMLCanvasElementWrapper;
-import com.github.xpenatan.gdx.backends.teavm.dom.HTMLImageElementWrapper;
 import com.github.xpenatan.gdx.backends.teavm.dom.StorageWrapper;
-import com.github.xpenatan.gdx.backends.teavm.dom.WindowWrapper;
-import com.github.xpenatan.gdx.backends.teavm.dom.XMLHttpRequestWrapper;
-import com.github.xpenatan.gdx.backends.teavm.dom.impl.TeaWindow;
 import org.teavm.jso.JSObject;
-import org.teavm.jso.ajax.XMLHttpRequest;
 import org.teavm.jso.browser.Storage;
 
 /**
