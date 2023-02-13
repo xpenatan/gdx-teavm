965a1441b18064bc1c910a8ccb2bb89a2e016dce
Natan <xpenatan@gmail.com>
2023-02-12 22:11:08 -0300

    remove old emu

--- backends/backend-web/backend-web/emuBk/emucom/badlogic/gdx/assets/loaders/FileHandleResolver.java
+++
@@ -1,7 +0,0 @@
-package emucom.badlogic.gdx.assets.loaders;
-
-import emucom.badlogic.gdx.files.FileHandle;
-
-public interface FileHandleResolver {
-    public FileHandle resolve (String fileName);
-}
