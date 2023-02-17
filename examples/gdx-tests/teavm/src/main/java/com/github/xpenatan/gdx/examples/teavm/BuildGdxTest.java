a54bf91eca17b11a88c1431eaef98a00ce07ec26
Natan <xpenatan@gmail.com>
2023-02-17 16:48:06 -0300

    Remove old tests

--- examples/gdx-tests/teavm/src/main/java/com/github/xpenatan/gdx/examples/teavm/BuildGdxTest.java
+++ examples/gdx-tests/teavm/src/main/java/com/github/xpenatan/gdx/examples/teavm/BuildGdxTest.java
@@ -3,6 +3,7 @@ package com.github.xpenatan.gdx.examples.teavm;
 import com.github.xpenatan.gdx.backends.teavm.TeaBuildConfiguration;
 import com.github.xpenatan.gdx.backends.teavm.TeaBuilder;
 import com.github.xpenatan.gdx.backends.teavm.gen.SkipClass;
+import com.github.xpenatan.gdx.backends.teavm.plugins.TeaReflectionSupplier;
 import com.github.xpenatan.gdx.examples.teavm.launcher.GdxTestLauncher;
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +13,9 @@ import org.teavm.tooling.TeaVMTool;
 public class BuildGdxTest {
 
     public static void main(String[] args) throws IOException {
+        String reflectionPackage = "com.badlogic.gdx.math";
+        TeaReflectionSupplier.addReflectionClass(reflectionPackage);
+
         // Change to your source asset directory
         String libgdxTestAssetsPath = "D:\\Dev\\Projects\\java\\Libgdx\\tests\\gdx-tests-android\\assets\\";
 
@@ -20,6 +24,7 @@ public class BuildGdxTest {
         teaBuildConfiguration.webappPath = new File("build/dist").getCanonicalPath();
 
         TeaVMTool tool = TeaBuilder.config(teaBuildConfiguration);
+        tool.setObfuscated(false);
         tool.setMainClass(GdxTestLauncher.class.getName());
         TeaBuilder.build(tool);
     }
