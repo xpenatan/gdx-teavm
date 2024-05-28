2080b91152ecbe6306470e1b5dab1b73d413a21a
Natan <xpenatan@gmail.com>
2024-05-28 20:14:39 -0300

    remove comment from adding directory asset

--- backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/config/AssetsCopy.java
+++ backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/config/AssetsCopy.java
@@ -245,7 +245,7 @@ public class AssetsCopy {
 
     private static void copyDirectory(FileWrapper sourceDir, FileWrapper destDir, AssetFilter filter, ArrayList<Asset> assets) {
         if(!filter.accept(destDir.path(), true)) return;
-        // assets.add(new Asset(destDir, AssetType.Directory));
+        assets.add(new Asset(destDir, AssetType.Directory));
         destDir.mkdirs();
         FileWrapper[] files = sourceDir.list();
         for(int i = 0, n = files.length; i < n; i++) {
