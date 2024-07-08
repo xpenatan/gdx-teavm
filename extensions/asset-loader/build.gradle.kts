000df65f82ff29ef2e3624f80e51909bcc23c634
Natan <xpenatan@gmail.com>
2024-07-08 16:28:00 -0300

    Remove unused dependencies

--- extensions/asset-loader/build.gradle.kts
+++ extensions/asset-loader/build.gradle.kts
@@ -2,12 +2,7 @@ val moduleName = "asset-loader"
 
 dependencies {
     implementation("com.badlogicgames.gdx:gdx:${LibExt.gdxVersion}")
-
-    implementation("org.teavm:teavm-core:${LibExt.teaVMVersion}")
     implementation("org.teavm:teavm-classlib:${LibExt.teaVMVersion}")
-    implementation("org.teavm:teavm-jso:${LibExt.teaVMVersion}")
-    implementation("org.teavm:teavm-jso-apis:${LibExt.teaVMVersion}")
-    implementation("org.teavm:teavm-jso-impl:${LibExt.teaVMVersion}")
 }
 
 publishing {
