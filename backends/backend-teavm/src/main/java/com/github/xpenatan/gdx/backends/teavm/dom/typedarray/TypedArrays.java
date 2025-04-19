1d8d49fe96a202a6cdcbcc750cd9ddf6309f18d0
Natan <xpenatan@gmail.com>
2025-04-19 19:25:36 -0300

    Move Gdx Pixmap code to native class name

--- backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/dom/typedarray/TypedArrays.java
+++ backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/dom/typedarray/TypedArrays.java
@@ -164,10 +164,16 @@ public class TypedArrays {
     }
 
     public static void copy(Int8Array in, ByteBuffer out) {
-        int length = in.getLength();
-        for(int i = 0; i < length; i++) {
-            byte value = in.get(i);
-            out.put(i, value);
-        }
+//        if(PlatformDetector.isJavaScript() || out.isDirect()) {
+//            Int8Array array = Int8Array.fromJavaBuffer(out);
+//            array.set(in);
+//        }
+//        else {
+            int length = in.getLength();
+            for(int i = 0; i < length; i++) {
+                byte value = in.get(i);
+                out.put(i, value);
+            }
+//        }
     }
 }
