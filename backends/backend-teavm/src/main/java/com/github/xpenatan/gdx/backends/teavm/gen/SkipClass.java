aca518287df3c54e300026062c5cbd8e0775d8a0
Natan <xpenatan@gmail.com>
2025-04-26 10:48:17 -0300

    Re add skip class but keep it deprecated

---
+++ backends/backend-teavm/src/main/java/com/github/xpenatan/gdx/backends/teavm/gen/SkipClass.java
@@ -0,0 +1,12 @@
+package com.github.xpenatan.gdx.backends.teavm.gen;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Deprecated
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface SkipClass {
+}
