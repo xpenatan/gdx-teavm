689b6b87c2d3c90082a15f40a3623442edd0ef59
Natan <xpenatan@gmail.com>
2025-10-30 07:54:43 -0300

    Update freetype build

--- examples/freetype/teavm/src/main/java/com/github/xpenatan/gdx/examples/teavm/BuildFreetypeTest.java
+++ examples/freetype/teavm/src/main/java/com/github/xpenatan/gdx/examples/teavm/BuildFreetypeTest.java
@@ -3,20 +3,18 @@ package com.github.xpenatan.gdx.examples.teavm;
 import com.github.xpenatan.gdx.backends.teavm.config.AssetFileHandle;
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuildConfiguration;
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuilder;
-import com.github.xpenatan.gdx.backends.teavm.config.TeaTargetType;
 import java.io.File;
 import java.io.IOException;
+import org.teavm.tooling.TeaVMTargetType;
 import org.teavm.tooling.TeaVMTool;
 import org.teavm.vm.TeaVMOptimizationLevel;
 
 public class BuildFreetypeTest {
 
     public static void main(String[] args) throws IOException {
-        TeaBuildConfiguration teaBuildConfiguration = new TeaBuildConfiguration();
+        TeaBuildConfiguration teaBuildConfiguration = new TeaBuildConfiguration(TeaVMTargetType.JAVASCRIPT);
         teaBuildConfiguration.assetsPath.add(new AssetFileHandle("../desktop/assets"));
         teaBuildConfiguration.webappPath = new File("build/dist").getCanonicalPath();
-
-        teaBuildConfiguration.targetType = TeaTargetType.JAVASCRIPT;
         TeaBuilder.config(teaBuildConfiguration);
         TeaVMTool tool = new TeaVMTool();
         tool.setOptimizationLevel(TeaVMOptimizationLevel.SIMPLE);
