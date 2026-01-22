41d61c8af51fd80f8c58e9d082aa8bda1f42f509
Natan <xpenatan@gmail.com>
2026-01-21 23:47:01 -0300

    Update to lwjgl3.

--- examples/basic/desktop/src/main/java/Main.java
+++ examples/basic/desktop/src/main/java/Main.java
@@ -1,13 +1,19 @@
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.github.xpenatan.gdx.teavm.examples.basic.tests.webgl.Basic3DTest;
 import com.github.xpenatan.gdx.teavm.examples.basic.tests.webgl.SpriteBatchTest;
+import com.github.xpenatan.gdx.teavm.examples.basic.tests.webgl.SpriteBatchTest2;
 
 public class Main {
 
     public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.useVsync(false);
+        config.setForegroundFPS(0);
 //        new LwjglApplication(new GearsDemo());
 //        new LwjglApplication(new Basic3DTest());
-        new LwjglApplication(new SpriteBatchTest());
+//        new LwjglApplication(new SpriteBatchTest());
+        new Lwjgl3Application(new SpriteBatchTest2(), config);
 //        new LwjglApplication(new TeaVMInputTest());
 //        new LwjglApplication(new ReflectionTest());
 //        new LwjglApplication(new ReadPixelsTest());
--- examples/basic/teavm-glfw/src/main/java/TestCLauncher.java
+++ examples/basic/teavm-glfw/src/main/java/TestCLauncher.java
@@ -1,14 +1,21 @@
+import com.github.xpenatan.gdx.teavm.backends.glfw.GLFWApplicationConfiguration;
 import com.github.xpenatan.gdx.teavm.examples.basic.tests.webgl.GearsDemo;
 import com.github.xpenatan.gdx.teavm.backends.glfw.GLFWApplication;
 import com.github.xpenatan.gdx.teavm.examples.basic.tests.webgl.SpriteBatchTest;
+import com.github.xpenatan.gdx.teavm.examples.basic.tests.webgl.SpriteBatchTest2;
 
 public class TestCLauncher {
 
     public static void main(String[] args) {
+        GLFWApplicationConfiguration config = new GLFWApplicationConfiguration();
+        config.useVsync(false);
+        config.setForegroundFPS(0);
+
         System.setProperty("os.name", "Windows"); // TODO figure out how to obtain OS name
 //        new GLFWApplication(new TestCApplication());
 //        new GLFWApplication(new GearsDemo());
-        new GLFWApplication(new SpriteBatchTest());
+//        new GLFWApplication(new SpriteBatchTest());
+        new GLFWApplication(new SpriteBatchTest2(), config);
 //        new GLFWApplication(new ReadPixelsTest());
 //        new GLFWApplication(new Basic3DTest());
 //        new GLFWApplication(new HelloTriangle());
