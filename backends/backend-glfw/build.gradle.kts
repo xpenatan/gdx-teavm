94e4baf92f2a72287c594cc9368ac908253d55fb
Natan <xpenatan@gmail.com>
2026-01-24 19:39:31 -0300

    Remove jParser

--- backends/backend-glfw/build.gradle.kts
+++ backends/backend-glfw/build.gradle.kts
@@ -9,6 +9,4 @@ sourceSets["main"].java.setSrcDirs(mutableSetOf("emu", "src/main/java/"))
 dependencies {
     implementation("com.badlogicgames.gdx:gdx:${LibExt.gdxVersion}")
     api(project(":backends:backend-shared"))
-    api("com.github.xpenatan.jParser:jParser-core:${LibExt.jParserVersion}")
-    api("com.github.xpenatan.jParser:jParser-build:${LibExt.jParserVersion}")
 }
