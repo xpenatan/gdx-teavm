6ca8291f01b39cebae8986bb212c7ba4ed6791ba
Natan <xpenatan@gmail.com>
2026-01-28 20:18:46 -0300

    Set default value for logMemory to false

--- backends/backend-psp/src/main/java/com/github/xpenatan/gdx/teavm/backends/psp/PSPApplicationConfiguration.java
+++ backends/backend-psp/src/main/java/com/github/xpenatan/gdx/teavm/backends/psp/PSPApplicationConfiguration.java
@@ -8,6 +8,6 @@ public class PSPApplicationConfiguration {
      *  Logs Memory Usage in the PSP debug console.
      *  Must compile with {@link TeaPSPBackend#debugMemory} true
      */
-    public boolean logMemory = true;
+    public boolean logMemory = false;
     public int logMemoryDelayMilli = 3000;
 }
