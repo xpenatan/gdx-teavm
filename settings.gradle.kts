f2e307b5cc3ce8703423982a89525b0aa4992a4f
Natan <xpenatan@gmail.com>
2026-02-01 13:26:44 -0300

    Rename the example module teavm to teavm-web

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ include(":extensions:gdx-freetype-teavm")
 
 include(":examples:basic:core")
 include(":examples:basic:desktop")
-include(":examples:basic:teavm")
+include(":examples:basic:teavm-web")
 include(":examples:basic:teavm-glfw")
 include(":examples:basic:teavm-psp")
 
