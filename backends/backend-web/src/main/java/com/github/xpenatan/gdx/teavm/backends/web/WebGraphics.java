bc9b1d1e01cca1a058e431ac203f407a1dc2ce67
Natan <xpenatan@gmail.com>
2026-03-15 14:40:14 -0300

    Remove deprecated method

--- backends/backend-web/src/main/java/com/github/xpenatan/gdx/teavm/backends/web/WebGraphics.java
+++ backends/backend-web/src/main/java/com/github/xpenatan/gdx/teavm/backends/web/WebGraphics.java
@@ -63,23 +63,6 @@ public abstract class WebGraphics implements Graphics {
     public void end() {
     }
 
-    /**
-     * @deprecated use {@link #begin()} instead
-     */
-    @Deprecated
-    public void update() {
-        long currTimeStamp = System.currentTimeMillis();
-        deltaTime = (currTimeStamp - lastTimeStamp) / 1000.0f;
-        lastTimeStamp = currTimeStamp;
-        time += deltaTime;
-        frames++;
-        if(time > 1) {
-            this.fps = frames;
-            time = 0;
-            frames = 0;
-        }
-    }
-
     @Override
     public boolean isGL30Available() {
         return false;
